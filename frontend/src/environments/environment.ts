571d47cceb2f1c55080d5891328e088423095534
Debora wendy <deboranawendy14@gmail.com>
2026-05-10 16:49:42 +0100

    feat: configurar styles globais, app.config e environments

---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost/chocolate-shop/backend',
+  exchangeRateApiKey: 'SUA_CHAVE_AQUI',
+  exchangeRateApiUrl: 'https://v6.exchangerate-api.com/v6'
+};
